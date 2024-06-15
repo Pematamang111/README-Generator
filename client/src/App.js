e3698154b007b8c1263dc4022e496cce2fa2583d
Pema Sherpa <pematamang111@gmail.com>
2024-06-14 21:56:35 -0400

    add bootstrap css in app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+
 import NavBar from './components/NavBar';
-import Cart from "./components/Cart";
-import Home from "./components/Home";
+import Home from './components/Home';
+import Cart from './components/Cart';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-      <Switch>
-        <Route path='/cart' component={Cart} />
-        <Route path='/' component={Home}/>
-      </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+    <NavBar/>
+    <Home/>
+    <Cart/>
+    </>
   );
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,12 @@
 const Home = () => {
-    return ( <h2>Home</h2> );
-}
- 
+	return (
+		<div className="App">
+			<div className="container">
+                <br />
+				<h2 className="text-center">Stripe Payment</h2>
+			</div>
+		</div>
+	);
+};
+
 export default Home;
