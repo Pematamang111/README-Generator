7af56d4e2610871ebcb631879c03672796e81f34
Pema Sherpa <pematamang111@gmail.com>
2024-06-15 21:29:53 -0400

    connect stripe dashboard

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,4 @@
+import StripeCheckout from 'react-stripe-checkout';
 const Home = () => {
 	return (
 		<div className="App">
@@ -5,6 +6,17 @@ const Home = () => {
                 <br />
 				<h2 className="text-center">Stripe Payment</h2>
 			</div>
+           <div className="form-group container">
+            <StripeCheckout
+            className="center"
+            stripeKey='pk_live_51PS6KvBvIgfOOt5OvGYJ54mloUrIfq8fP6mwp4OSsby6e6B2UflQ2vspEe6w9Iufokw4wgkHxmteF606cdW8SeF800aVSNRk1K'
+           
+           
+           />
+            
+           </div>
+
+
 		</div>
 	);
 };
