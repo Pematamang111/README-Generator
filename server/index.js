0e72251bf323fddbb70d3f2acde59727756c0add
Pema Sherpa <pematamang111@gmail.com>
2024-06-19 21:52:10 -0400

    add server folder with index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const stripe = require('stripe')('')
+const stripe = require('stripe')
 const uuid = require('uuid').v4;
 const cors = require('cors');
 
