7ff665f0afd8b87c7566b08e9a4f629def5e8750
Pema Sherpa <pematamang111@gmail.com>
2024-06-19 21:45:25 -0400

    add server folder with index.js to run stripe

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const stripe = require('stripe')('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')
+const stripe = require('stripe')('')
+//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
 const uuid = require('uuid').v4;
 const cors = require('cors');
 
