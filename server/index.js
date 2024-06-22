d17ec1ca7b2cf5f0ccdf99bb90aed7948cd36296
Pema Sherpa <pematamang111@gmail.com>
2024-06-21 22:16:46 -0400

    add route for checkout

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const stripe = require('stripe')
+const stripe = require('stripe');
 const uuid = require('uuid').v4;
 const cors = require('cors');
 
@@ -9,9 +9,43 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 const PORT = process.env.PORT || 5000;
-app.post('/checkout', (req, res) => {
-  console.log(req.body);
-})
+app.post('/checkout', async (req, res) => {
+	console.log(req.body);
+	let error, status;
+	try {
+		const customer = await stripe.customers.create({
+			email: token.email,
+			source: token.id,
+		});
+		const key = uuid();
+		const charge = await stripe.charges.create(
+			{
+				amount: product.price * 100,
+				currency: 'usd',
+				receipt_email: token.email,
+				description: `Purchased the ${product.name}`,
+				shipping: {
+					name: token.card.name,
+					address: {
+						line1: token.card.address_line1,
+						line2: token.card.address_line2,
+						city: token.card.address_city,
+						country: token.card.address_country,
+						postal_code: token.card.address_zip,
+					},
+				},
+			},
+			{
+				key,
+			}
+		);
+		console.log('charge:', { charge }), (status = 'success');
+	} catch (error) {
+		conaole.log(error);
+		status = 'failure';
+	}
+	res.json({ error, status });
+});
 
 app.listen(PORT, () => {
 	console.log('App is listening on port 5000');
